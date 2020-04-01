4c19deb6cda188d7fb2ba7d2676c0941516da291
Fernando Junior <nandomagalhaesrj@gmail.com>
2020-04-01 12:46:10 -0300

    Implementaçao da RegExp no filtro de busca da tabela

--- js/filtra.js
+++ js/filtra.js
@@ -9,8 +9,10 @@ campoFiltro.addEventListener('input', function() {
       var paciente = pacientes[i];
       var tdNome = paciente.querySelector('.info-nome');
       var nome = tdNome.textContent;
+
+      var expressao = new RegExp(this.value, 'i');
   
-      if(nome != this.value) {
+      if(!expressao.test(nome)) {
         paciente.classList.add('invisivel');
       }
       else {
